c8f09f7f28479263e71e68f2a014abab58f156ac
Sreechandh Devireddy <115910847+Sreechandh22@users.noreply.github.com>
2024-09-29 19:17:56 -0700

    Update index.js
    Signed-off-by: Sreechandh Devireddy <115910847+Sreechandh22@users.noreply.github.com>

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -71,7 +71,7 @@ export const Home = () => {
                       <div className="ring three"></div>
                     </div>
                   </Link>
-                  <a href="https://drive.google.com/file/d/1ey-Luysv0PFP5ffel_F0BkQlhJcujQlo/view" target="_blank">
+                  <a href="https://drive.google.com/file/d/1qGyq4bRazqdzeXTQ6yrKGJA28Q_4Rga3/view?usp=sharing" target="_blank">
                   <div id="button_h" className="ac_btn btn">
                       Resume
                       <div className="ring one"></div>

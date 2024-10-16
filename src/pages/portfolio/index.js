cc02f933bcb23d7eade3e7049789db335a2de9d5
Sreechandh Devireddy <115910847+Sreechandh22@users.noreply.github.com>
2024-10-16 01:43:49 -0700

    Update index.js
    Signed-off-by: Sreechandh Devireddy <115910847+Sreechandh22@users.noreply.github.com>

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -23,7 +23,7 @@ export const Portfolio = () => {
           {dataportfolio.map((data, i) => {
             return (
               <div key={i} className="po_item">
-                <img src={data.img} alt="" />
+                <img src={data.img} alt="" loading="lazy" />
                 <div className="content">
                   <p>{data.description}</p>
                   <a href={data.link}>view project</a>
